0ad05b1025465b86e967683f842eec1d33f83534
Pedro Ivo <7pedro.ivo@gmail.com>
2019-11-11 22:34:24 -0300

    Melhorias no service

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,37 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
-import { HeaderService } from './header.service'
-
-
-
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent extends AppComponent implements OnInit {
 
   constructor(
     private serviceHeaderService: HeaderService
   ) {
-    //super();
+    super();
   }
 
-  public responseTopicos;
+  public responseTopicos: JSON;
 
   ngOnInit() {
-    this.getItemAoVivo()
+    this.getAllTopics();
+    console.log(this.responseTopicos);
   }
 
-  getItemAoVivo() {
-    var mudabotao: any;
-
-    return this.serviceHeaderService.getTopicos()
-    /* .subscribe((data: any) => this.responseTopicos = data,
-      error => console.log("Erro na consulta", error),
-      () => { }
-    ); */
+  getAllTopics() {
+    return this.serviceHeaderService
+      .getTopicos()
+      .subscribe((data: any) =>
+        this.responseTopicos = data,
+        error => console.log("Erro na consulta", error),
+        () => { }
+      );
   }
 
 }
--- src/app/home/header/header.service.ts
+++ src/app/home/header/header.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { UrlConstantes } from '../../shared/url-constantes'
+import { ProjectService } from 'src/app/shared/services/project.service';
 
 @Injectable({
   providedIn: 'root'
 
 })
 
-export class HeaderService {
+export class HeaderService extends ProjectService {
 
-  constructor(protected http: HttpClient, httpClient: HttpClientModule) {
-    //super(http);
+  constructor(protected http: HttpClient) {
+    super(http);
   }
 
   public getTopicos() {
-    this.http.get(UrlConstantes.GET_TOPICOS).subscribe(response => {
-      console.log(response);
+
+
+    return super.getAll<any>(UrlConstantes.GET_TOPICOS)
+    /* return this.http.get(UrlConstantes.GET_TOPICOS).subscribe(response => {
     }, err => {
       console.log('Erro ao executar getTopicos: ' + err);
-    });
+    }); */
   }
+
 }
